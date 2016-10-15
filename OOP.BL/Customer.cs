50f58212230285d97c1073d4e1196129d17cf515
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-15 06:02:28 -0700

    Add code to Customer Class

--- OOP.BL/Customer.cs
+++ OOP.BL/Customer.cs
@@ -8,5 +8,43 @@ namespace OOP.BL
 {
     public class Customer
     {
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public static int InstanceCount { get; }
+
+        public string FullName
+        {
+            get
+            {
+                var fullName = LastName;
+
+                if (!String.IsNullOrWhiteSpace(LastName))
+                {
+                    if (!String.IsNullOrWhiteSpace(FirstName)
+                    {
+                        fullName += FirstName;
+                    }
+                }
+                return fullName;
+            }
+        }
     }
 }
