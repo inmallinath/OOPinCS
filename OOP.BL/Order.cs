b4e9a379256af411ebbe7805860536daebbc9dca
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-15 21:59:58 -0700

    Create a Order repository to manage the retrieve and save actions for the orders

--- OOP.BL/Order.cs
+++ OOP.BL/Order.cs
@@ -37,34 +37,6 @@ namespace OOP.BL
         public DateTimeOffset? OrderDate { get; set; }
 
         /// <summary>
-        /// Save the order to the DB
-        /// </summary>
-        /// <returns></returns>
-        public bool Save()
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// Retrieve an Order by providing the Order ID
-        /// </summary>
-        /// <param name="orderId"></param>
-        /// <returns></returns>
-        public Order Retrieve(int orderId)
-        {
-            return new Order();
-        }
-
-        /// <summary>
-        /// Retrieve a list of Orders
-        /// </summary>
-        /// <returns></returns>
-        public List<Order> Retrieve()
-        {
-            return new List<Order>();
-        }
-
-        /// <summary>
         /// Validate Method to verify that the Order Date is not Null
         /// </summary>
         /// <returns></returns>
---
+++ OOP.BL/OrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.BL
+{
+    public class OrderRepository
+    {
+        /// <summary>
+        /// Save the order to the DB
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieve an Order by providing the Order ID
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            var order = new Order(orderId);
+            if (orderId == 1)
+            {
+                order.OrderDate = new DateTimeOffset(2016, 10, 15, 08, 30, 00, new TimeSpan(-6, 0, 0));
+            }
+            return order;
+        }
+
+        /// <summary>
+        /// Retrieve a list of Orders
+        /// </summary>
+        /// <returns></returns>
+        public List<Order> Retrieve()
+        {
+            return new List<Order>();
+        }
+    }
+}
