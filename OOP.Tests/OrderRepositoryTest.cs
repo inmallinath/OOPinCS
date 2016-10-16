0ef9f9f649737b621115c33752c5780afedfeb70
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-15 22:05:38 -0700

    Create the Repository automated test for the retrieve action

---
+++ OOP.Tests/OrderRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP.BL;
+
+namespace OOP.Tests
+{
+    [TestClass]
+    public class OrderRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveExistingOrder()
+        {
+            //Arrange
+            var orderData = new OrderRepository();
+            var expected = new Order(1)
+            {
+                OrderDate = new DateTimeOffset(2016, 10, 15, 08, 30, 00, new TimeSpan(-6, 0, 0))
+            };
+
+            //Act
+            var actual = orderData.Retrieve(1);
+
+            //Assert
+            Assert.AreEqual(expected.OrderId, actual.OrderId);
+            Assert.AreEqual(expected.OrderDate, actual.OrderDate);
+        }
+    }
+}
