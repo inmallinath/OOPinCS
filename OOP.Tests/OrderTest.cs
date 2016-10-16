abab062b7f7b8b2fb7333aee33107024ca83cc18
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-15 17:54:02 -0700

    Add Test to the Order Class

---
+++ OOP.Tests/OrderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP.BL;
+
+namespace OOP.Tests
+{
+    [TestClass]
+    public class OrderTest
+    {
+        /// <summary>
+        /// Test for a Valid Order with an Order Date defined
+        /// </summary>
+        [TestMethod]
+        public void TestValidOrderDate()
+        {
+            //Arrange
+            var order = new Order();
+            var expected = false;
+
+            //Act
+            var actual = order.Validate();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
