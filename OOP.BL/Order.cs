6440914e578bd6d7bcd54dc925ec3012cd798888
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-15 17:48:22 -0700

    Create a new Order Class to manage orders

---
+++ OOP.BL/Order.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.BL
+{
+    public class Order
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Order()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor that allows setting of OrderId
+        /// </summary>
+        /// <param name="orderId"></param>
+        public Order(int orderId)
+        {
+            this.OrderId = orderId;
+        }
+
+        /// <summary>
+        /// Auto-Implemented Properties
+        /// </summary>
+        public int OrderId { get; private set; }
+
+        /// <summary>
+        /// DateTimeOffset provides the time offset for saving the date with time and offset
+        /// To manage timezones
+        /// </summary>
+        public DateTimeOffset? OrderDate { get; set; }
+
+        /// <summary>
+        /// Save the order to the DB
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieve an Order by providing the Order ID
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        /// <summary>
+        /// Validate Method to verify that the Order Date is not Null
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null)
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
+    }
+}
