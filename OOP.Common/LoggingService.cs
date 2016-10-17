334d75b99fa4b0ab796118831e833d2aaaf4fe8a
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-16 20:58:55 -0700

    Write a test for Logging Service method

--- OOP.Common/LoggingService.cs
+++ OOP.Common/LoggingService.cs
@@ -8,11 +8,11 @@ namespace OOP.Common
 {
     public class LoggingService
     {
-        public static void WriteToFile(List<object> changedItems)
+        public static void WriteToFile(List<ILoggable> changedItems)
         {
             foreach (var item in changedItems)
             {
-                //Console.WriteLine(item.);
+                Console.WriteLine(item.Log());
             }
         }
     }
---
+++ OOP.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP.Common;
+using OOP.BL;
+using System.Collections.Generic;
+
+namespace OOP.CommonTest
+{
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            //Arrange
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                EmailAddress = "test@csharp.com",
+                FirstName = "CSharp",
+                LastName = "Test",
+                AddressList = null
+            };
+            changedItems.Add(customer as ILoggable);
+
+            var product = new Product(1)
+            {
+                ProductName = "Terminal SP-800",
+                ProductDescription = "End user terminal",
+                CurrentPrice = 100.4m
+            };
+            changedItems.Add(product as ILoggable);
+
+            //Act
+            LoggingService.WriteToFile(changedItems);
+            //Assert
+            //NOT NEEDED
+        }
+    }
+}
